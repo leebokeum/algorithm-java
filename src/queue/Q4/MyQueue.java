fe8c1c604cab017a0a355786b4e3996bda4328a9
bokeum.lee <bokeum.lee@navercorp.com>
2024-02-05 00:35:16 +0900

    Q4 주석 추가

--- src/queue/Q4/MyQueue.java
+++ src/queue/Q4/MyQueue.java
@@ -2,41 +2,68 @@ package queue.Q4;
 
 import java.util.Stack;
 
+/**
+ * 두개의 스택으로 Queue 구현하기
+ */
 public class MyQueue<T>  {
+
+    // 두개의 스택
     Stack<T> stackNewest, stackOldest;
 
+    // 생성시 두개의 스택 초기화
     public MyQueue() {
         stackNewest = new Stack<T>();
         stackOldest = new Stack<T>();
     }
 
+    /**
+     * 큐의 전체 사이즈 : 두개의 스택 사이즈를 더하면 전체 큐 사이즈가 된다.
+     */
     public int size() {
         return stackNewest.size() + stackOldest.size();
     }
 
+    /**
+     * 큐에 값 추가
+     */
     public void add(T value) {
-        // Push onto stack1
+        // 값을 추가할 때는 첫번째 스택(stackNewest)에만 쌓는다.
         stackNewest.push(value);
     }
 
-    /* Move elements from stackNewest into stackOldest. This is usually done so that we can
-     * do operations on stackOldest.
+    /**
+     * 스택의 값 옮기기
+     * stackNewest 스택에서 stackOldest 로 값을 이동 시킨다.
      */
     private void shiftStacks() {
+        // stackOldest 에 값이 비어있을 때만 수행한다.
         if (stackOldest.isEmpty()) {
+            // stackNewest 에 있는 값 전부를 stackOldest 에 이동 시킨다.
             while (!stackNewest.isEmpty()) {
                 stackOldest.push(stackNewest.pop());
             }
         }
     }
 
+    /**
+     * 큐에 가장 앞에 있는 값 보기
+     */
     public T peek() {
+        // 1. 스택의 값을 옮기고
         shiftStacks();
-        return stackOldest.peek(); // retrieve the oldest item.
+
+        // 2. stackOldest 의 가장 위(top)에 있는 값을 보여준다.
+        return stackOldest.peek();
     }
 
+    /**
+     * 큐의 값 꺼내기
+     */
     public T remove() {
+        // 1. 스택의 값을 옮기고
         shiftStacks();
-        return stackOldest.pop(); // pop the oldest item.
+
+        // 2. stackOldest 의 가장 위(top)에 있는 값을 꺼낸다.
+        return stackOldest.pop();
     }
 }
--- src/queue/Q4/Q4.java
+++ src/queue/Q4/Q4.java
@@ -1,9 +1,5 @@
 package queue.Q4;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Q4 {
     /**
      * 스택으로 큐: 스택 두 개로 큐 하나를 구현한 MyQueue 클래스를 구현하라.
@@ -11,33 +7,13 @@ public class Q4 {
     public static void main(String[] args) {
         MyQueue<Integer> my_queue = new MyQueue<Integer>();
 
-        // Let's test our code against a "real" queue
-        Queue<Integer> test_queue = new LinkedList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            my_queue.add(i);
+            System.out.println("Enqueued " + i);
+        }
 
-        for (int i = 0; i < 100; i++) {
-            int choice = ThreadLocalRandom.current().nextInt(0, 10);
-            if (choice <= 5) { // enqueue
-                int element = ThreadLocalRandom.current().nextInt(1, 10);
-                test_queue.add(element);
-                my_queue.add(element);
-                System.out.println("Enqueued " + element);
-            } else if (test_queue.size() > 0) {
-                int top1 = test_queue.remove();
-                int top2 = my_queue.remove();
-                if (top1 != top2) { // Check for error
-                    System.out.println("******* FAILURE - DIFFERENT TOPS: " + top1 + ", " + top2);
-                }
-                System.out.println("Dequeued " + top1);
-            }
-
-            if (test_queue.size() == my_queue.size()) {
-                if (test_queue.size() > 0 && test_queue.peek() != my_queue.peek()) {
-                    System.out.println(
-                        "******* FAILURE - DIFFERENT TOPS: " + test_queue.peek() + ", " + my_queue.peek() + " ******");
-                }
-            } else {
-                System.out.println("******* FAILURE - DIFFERENT SIZES ******");
-            }
+        for (int k = 0; k < 5; k++) {
+            System.out.println("Dequeued " + my_queue.remove());
         }
     }
 }
